a25ba4fd5d7a6dc7e86b18bd169a9581e5b85d12
ElyA_I <elyaizmaylova@gmail.com>
2023-01-01 19:24:45 +0300

    Адаптация под планшетную версию ч.1

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src(['./sass/**/*.scss', './sass/**/*.sass'])
+  return src(['sass/**/*.scss', 'sass/**/*.sass'])
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(dest('./dist/css'));
-};
+    .pipe(dest('dist/css'));
+}
 
-exports.default = series(buildStyles);
+function buildFonts() {
+  return src('fonts/*').pipe(dest('dist/fonts'));
+}
 
-exports.watch = function () {
-  watch('./sass/**/*.scss', ['sass']);
-};
+exports.default = series(buildStyles, buildFonts);
